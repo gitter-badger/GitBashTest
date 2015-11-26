de82e5d6e53c1b0e4174e0f732b89a19112cf162
Móricz László <lmoricz@Moricz-iMac.local>
2015-11-26 12:08:54 +0100

    Adding some files...

---
+++ GitBashTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitBashTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tempreadline;
+            Console.WriteLine("GitBashTest");
+            tempreadline = Console.ReadLine();
+        }
+    }
+}
